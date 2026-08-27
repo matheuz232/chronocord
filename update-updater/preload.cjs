c3b1ca6acd2ef342df48b439038ec7689a3eb06a
matheuz232 <82462256+matheuz232@users.noreply.github.com>
2026-08-27 19:08:55 -0300

    cleanup: remove obsolete Electron helper sources

--- update-updater/preload.cjs
+++
@@ -1,2 +0,0 @@
-const {contextBridge,ipcRenderer}=require('electron');
-contextBridge.exposeInMainWorld('updaterAPI',{onEvent:(cb)=>{const h=(_,d)=>cb(d);ipcRenderer.on('updater:event',h);return()=>ipcRenderer.removeListener('updater:event',h)}});
