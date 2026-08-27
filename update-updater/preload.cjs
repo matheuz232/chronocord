af4f7709cc9b0df9547339fadf5afbd0d25c88b9
matheuz232 <82462256+matheuz232@users.noreply.github.com>
2026-08-26 23:41:01 -0300

    polish: reduce updater preload surface

--- update-updater/preload.cjs
+++ update-updater/preload.cjs
@@ -1,2 +1,2 @@
 const {contextBridge,ipcRenderer}=require('electron');
-contextBridge.exposeInMainWorld('updaterAPI',{update:()=>ipcRenderer.invoke('update-now'),later:()=>ipcRenderer.invoke('later'),onEvent:(cb)=>{const h=(_,d)=>cb(d);ipcRenderer.on('updater:event',h);return()=>ipcRenderer.removeListener('updater:event',h)}});
+contextBridge.exposeInMainWorld('updaterAPI',{onEvent:(cb)=>{const h=(_,d)=>cb(d);ipcRenderer.on('updater:event',h);return()=>ipcRenderer.removeListener('updater:event',h)}});
